a53c1f1fd02cad91ac60634d5e3e02a47076d765
Jonas Heinle <30556580+Kataglyphis@users.noreply.github.com>
2022-04-24 11:03:16 +0200

    exluding all sub dirs from language analysis

--- Src/scene/Vertex.cpp
+++ Src/scene/Vertex.cpp
@@ -2,6 +2,11 @@
 
 Vertex::Vertex()
 {
+	this->pos				= glm::vec3(-1.f);
+	this->normal			= glm::vec3(-1.f);
+	this->color				= glm::vec3(-1.f);
+	this->mat_id			= glm::vec3(-1.f);
+	this->texture_coords	= glm::vec3(-1.f);
 }
 
 Vertex::Vertex(glm::vec3 pos, glm::vec3 normal, glm::vec3 color, glm::vec3 mat_id, glm::vec2 texture_coords)
