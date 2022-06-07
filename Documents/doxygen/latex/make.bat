0fb7895fc38e8c91b810117d471b97f494de76df
Jonas Heinle <jonasheinle@googlemail.com>
2022-06-07 17:28:02 +0200

    no cppcheck osdfutput files

--- Documents/doxygen/latex/make.bat
+++ Documents/doxygen/latex/make.bat
@@ -7,7 +7,7 @@ set ORG_MKIDX_CMD=%MKIDX_CMD%
 set ORG_BIBTEX_CMD=%BIBTEX_CMD%
 set ORG_LATEX_COUNT=%LATEX_COUNT%
 set ORG_MANUAL_FILE=%MANUAL_FILE%
-if "X"%LATEX_CMD% == "X" set LATEX_CMD=latex
+if "X"%LATEX_CMD% == "X" set LATEX_CMD=pdflatex
 if "X"%MKIDX_CMD% == "X" set MKIDX_CMD=makeindex
 if "X"%BIBTEX_CMD% == "X" set BIBTEX_CMD=bibtex
 if "X"%LATEX_COUNT% == "X" set LATEX_COUNT=8
@@ -16,11 +16,14 @@ if "X"%MANUAL_FILE% == "X" set MANUAL_FILE=refman
 del /s /f *.ps *.dvi *.aux *.toc *.idx *.ind *.ilg *.log *.out *.brf *.blg *.bbl %MANUAL_FILE%.pdf
 
 
-%LATEX_CMD% %MANUAL_FILE%.tex
+%LATEX_CMD% %MANUAL_FILE%
 echo ----
 %MKIDX_CMD% %MANUAL_FILE%.idx
+echo ----
+%LATEX_CMD% %MANUAL_FILE%
+
 setlocal enabledelayedexpansion
-set count=%LAT#EX_COUNT%
+set count=%LATEX_COUNT%
 :repeat
 set content=X
 for /F "tokens=*" %%T in ( 'findstr /C:"Rerun LaTeX" %MANUAL_FILE%.log' ) do set content="%%~T"
@@ -31,14 +34,12 @@ set /a count-=1
 if !count! EQU 0 goto :skip
 
 echo ----
-%LATEX_CMD% %MANUAL_FILE%.tex
+%LATEX_CMD% %MANUAL_FILE%
 goto :repeat
 :skip
 endlocal
 %MKIDX_CMD% %MANUAL_FILE%.idx
-%LATEX_CMD% %MANUAL_FILE%.tex
-dvips -o %MANUAL_FILE%.ps %MANUAL_FILE%.dvi
-gswin32c.exe -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=%MANUAL_FILE%.pdf -c save pop -f %MANUAL_FILE%.ps
+%LATEX_CMD% %MANUAL_FILE%
 
 @REM reset environment
 cd /D %Dir_Old%
