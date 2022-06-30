b7077ecaa8f51b624eecbfdca8a8b968d3992300
Jonas Heinle <jonasheinle@googlemail.com>
2022-06-30 11:36:06 +0200

    fixed

--- include/renderer/VulkanRendererConfig.hpp
+++ include/renderer/VulkanRendererConfig.hpp
@@ -9,7 +9,7 @@
 #define VULKAN_VERSION_MAJOR "1"
 #define VULKAN_VERSION_MINOR "3"
 
-#define GLSLC_EXE "/bin/glslc"
+#define GLSLC_EXE "C:/VulkanSDK/1.3.211.0/Bin/glslc.exe"
 // change this path when install
 #define RELATIVE_RESOURCE_PATH "/../Resources/" // /..
 #define RELATIVE_INCLUDE_PATH "/../include/" // /..
